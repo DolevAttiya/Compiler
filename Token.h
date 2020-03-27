c98ba57aeb393f7e9eb61a45048dcf4ec842f553
Assaf Lerman <assaflerman@gmail.com>
2020-03-27 13:19:17 +0300

    Added Token.h

---
+++ Token.h
@@ -0,0 +1,46 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern FILE* yyin, * yyout;
+
+typedef enum eTOKENS
+{
+	INT_tok,
+	FLOAT_tok,
+	VOID_tok,
+	IF_tok,
+	RETURN_tok,
+	NUM_tok,
+	OP_tok,
+	ID_tok,
+	COMMA_tok,
+	COLON_tok,
+	SEMICOLON_tok,
+	PARENTHESES_tok,
+	BRACKETS_tok,
+	COMMENT_tok
+}eTOKENS;
+
+typedef struct Token
+{
+	eTOKENS kind;
+	char* lexeme;
+	int lineNumber;
+}Token;
+
+typedef struct Node
+{
+	Token* tokensArray;
+	struct Node* prev;
+	struct Node* next;
+} Node;
+
+void create_and_store_token(eTOKENS kind, char* lexeme, int numOfLine);
+Token* next_token();
+Token* back_token();
+
+#endif

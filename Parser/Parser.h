bde1f480f1bcbf0e86b47406d3cc08415ebd9113
Assaf Lerman <assaflerman@gmail.com>
2020-05-05 22:16:12 +0300

    Added Parser.h and changed the Parser project to be a static library

---
+++ Parser/Parser.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "../Lexical Analyzer/Token/Token.h"
+
+int match(eTOKENS token_type);
+int error(eTOKENS expected_token_type);
+
+int parse();
+int parse_PROG();
+int parse_GLOBAL_VARS();
+//int parse_GLOBAL_VARS_TAG();
+int parse_VAR_DEC();
+int parse_VAR_DEC_TAG();
+int parse_TYPE();
+int parse_DIM_SIZES();
+int parse_DIM_SIZES_TAG();
+//int parse_FUNC_PREDEFS();
+//int parse_FUNC_PREDEFS_TAG();
+int parse_FUNC_PROTOTYPE();
+int parse_FUNC_FULL_DEFS();
+int parse_FUNC_FULL_DEFS_TAG();
+int parse_FUNC_WITH_BODY();
+
+int parse_COMP_STMT(); //Didn't implement
+int parse_RETURN_TYPE(); //Didn't implement
+int parse_PARAMS(); //Didn't implement

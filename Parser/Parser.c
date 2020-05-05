e77c700691cb8977f2990b1ca376bee006aaf082
Assaf Lerman <assaflerman@gmail.com>
2020-05-05 22:11:30 +0300

    More implementations

--- Parser/Parser.c
+++ Parser/Parser.c
@@ -12,17 +12,36 @@ int parse()
 
 int parse_PROG()
 {
+	eTOKENS follow[] = { EOF_tok };
+	current_follow = follow;
 	parse_GLOBAL_VARS();
 	do {
 		for(int i=0;i<3;i++) 
 			current_token=next_token();
 		if (current_token->kind != PARENTHESIS_OPEN_tok)
+			for (int i = 0; i < 3; i++)
+				back_token();
 			parse_GLOBAL_VARS();
-		for (int i = 0; i < 3; i++)
-			back_token();
 	} while (current_token->kind != PARENTHESIS_OPEN_tok);
-	parse_FUNC_PREDEFS();
-	parse_FUNC_FULL_DEFS();
+	for (int i = 0; i < 3; i++)
+		back_token();
+
+	do {
+		parse_FUNC_PROTOTYPE();
+		current_token = next_token();
+	} while (current_token->kind == SEMICOLON_tok);
+
+	if (!match(BRACKET_OPEN_tok))
+		return 0;
+
+	back_token();       
+	parse_COMP_STMT();
+	current_token = next_token();
+	if (current_token->kind != EOF_tok)
+	{
+		back_token();    
+		parse_FUNC_FULL_DEFS();
+	}
 }
 
 int parse_GLOBAL_VARS()
@@ -48,10 +67,9 @@ int parse_VAR_DEC()
 	eTOKENS follow[] = { INT_tok, FLOAT_tok, VOID_tok, ID_tok, CURLY_BRACKET_OPEN_tok, IF_tok, RETURN_tok };
 	current_follow = follow;
 	parse_TYPE();
-	if (match(ID_tok))
-		parse_VAR_DEC_TAG();
-	else
+	if (!match(ID_tok))
 		return 0;
+	parse_VAR_DEC_TAG();	
 }
 
 int parse_VAR_DEC_TAG()
@@ -65,8 +83,10 @@ int parse_VAR_DEC_TAG()
 		break;
 	case BRACKET_OPEN_tok:
 		parse_DIM_SIZES();
-		match(BRACKET_CLOSE_tok);
-		match(SEMICOLON_tok);
+		if (!match(BRACKET_CLOSE_tok))
+			return 0;
+		if (!match(SEMICOLON_tok))
+			return 0;
 		break;
 	default:
 		error(expected_token_type);
@@ -95,7 +115,8 @@ int parse_DIM_SIZES()
 {
 	eTOKENS follow[] = { BRACKET_CLOSE_tok };
 	current_follow = follow;
-	match(INT_NUM_tok);
+	if (!match(INT_NUM_tok))
+		return 0;
 	parse_DIM_SIZES_TAG();
 }
 
@@ -107,7 +128,6 @@ int parse_DIM_SIZES_TAG()
 	switch (current_token->kind)
 	{
 	case COMMA_tok:
-		back_token();
 		parse_DIM_SIZES();
 		break;
 	case BRACKET_CLOSE_tok:
@@ -119,49 +139,50 @@ int parse_DIM_SIZES_TAG()
 	}
 }
 
-int parse_FUNC_PREDEFS()
-{
-	eTOKENS follow[] = { INT_tok, FLOAT_tok, VOID_tok };
-	current_follow = follow;
-	parse_FUNC_PROTOTYPE();
-	match(SEMICOLON_tok);
-	parse_FUNC_FULL_DEFS_TAG();
-}
-
-int parse_FUNC_PREDEFS_TAG()
-{
-
-	parse_FUNC_PROTOTYPE();
-	match(SEMICOLON_tok);
-
-}
-
 int parse_FUNC_PROTOTYPE()
 {
-
+	eTOKENS follow[] = { SEMICOLON_tok, CURLY_BRACKET_OPEN_tok };
+	current_follow = follow;
+	parse_RETURN_TYPE();
+	if (!match(ID_tok))
+		return 0;
+	if (!match(PARENTHESIS_OPEN_tok))
+		return 0;
+	parse_PARAMS();
+	if (!match(PARENTHESIS_CLOSE_tok))
+		return 0;
 }
 
 int parse_FUNC_FULL_DEFS()
 {
-
+	parse_FUNC_WITH_BODY();
+	parse_FUNC_FULL_DEFS_TAG();
 }
 
 int parse_FUNC_FULL_DEFS_TAG()
 {
-
+	eTOKENS follow[] = { EOF_tok };
+	current_follow = follow;
+	current_token = next_token();
+	switch (current_token->kind)
+	{
+	case INT_tok:
+	case FLOAT_tok:
+	case VOID_tok:
+		back_token();
+		parse_FUNC_FULL_DEFS();
+		break;
+	case EOF_tok:
+		back_token();
+		break;
+	default:
+		error(expected_token_type);
+		break;
+	}
 }
 
 int parse_FUNC_WITH_BODY()
 {
-
-}
-
-int match(eTOKENS token_type) 
-{
-
-}
-
-int error(eTOKENS expected_token_type)
-{
-
+	parse_FUNC_PROTOTYPE();
+	parse_COMP_STMT();
 }

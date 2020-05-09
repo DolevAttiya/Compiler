528160e4ab5ea01543d8111d23f3126241e80f76
DolevAttiya <50592453+DolevAttiya@users.noreply.github.com>
2020-05-09 13:15:11 +0300

    Implement Parse Follow

--- Parser/Parser.c
+++ Parser/Parser.c
@@ -1,1 +1,18 @@
 #include "../Lexical Analyzer/Token/Token.h"
+#include "Parser.h"
+
+Token* current_token;
+eTOKENS* current_follow;
+int current_follow_size;
+eTOKENS expected_token_type;
+FILE* parser_output_file;
+
+int parse_Follow()
+{
+	int flag = 0;
+	for (int i = 0; i < current_follow_size; i++)
+	{
+		flag += current_token->kind == current_follow[i];
+	}
+	return flag;
+}

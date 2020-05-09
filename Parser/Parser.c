b15b55779efc0847de21c7b736976ad56bda4db6
DolevAttiya <50592453+DolevAttiya@users.noreply.github.com>
2020-05-09 13:17:30 +0300

    Implement Error

--- Parser/Parser.c
+++ Parser/Parser.c
@@ -1,1 +1,18 @@
 #include "../Lexical Analyzer/Token/Token.h"
+#include "Parser.h"
+
+Token* current_token;
+eTOKENS* current_follow;
+int current_follow_size;
+eTOKENS expected_token_type;
+FILE* parser_output_file;
+
+int error(eTOKENS expected_token_type) {
+	fprintf(parser_output_file, "Expected token of type '{%s}' at line: {%d},  Actual token of type '{%s}', lexeme: '{%s}'.", expected_token_type, current_token->lineNumber, current_token->kind, current_token->lexeme);
+	do
+	{
+		current_token = next_token();
+	} while (parse_Follow() != 0 || current_token == EOF_tok);
+	back_token();
+
+}

56fd46a143be03a364333ab2d8a85b2925aad12d
DolevAttiya <50592453+DolevAttiya@users.noreply.github.com>
2020-05-09 13:12:25 +0300

    fix headers

--- Parser/Parser.h
+++ Parser/Parser.h
@@ -1,9 +1,10 @@
 #pragma once
 #include "../Lexical Analyzer/Token/Token.h"
 
+FILE* parser_output_file;
 int match(eTOKENS token_type);
 int error(eTOKENS expected_token_type);
-FILE* parser_output_file;
+int parse_Follow();
 
 int parse();
 int parse_PROG();
@@ -21,6 +22,24 @@ int parse_FUNC_FULL_DEFS();
 int parse_FUNC_FULL_DEFS_TAG();
 int parse_FUNC_WITH_BODY();
 
-int parse_COMP_STMT(); //Didn't implement
-int parse_RETURN_TYPE(); //Didn't implement
-int parse_PARAMS(); //Didn't implement
+int parse_RETURN_TYPE();
+int parse_PARAMS();
+int parse_PARAM_LIST();
+int parse_PARAM_LIST_TAG();
+int parse_PARAM();
+int parse_PARAM_TAG();
+int parse_COMP_STMT();
+int parse_VAR_DEC_LIST();
+int parse_VAR_DEC_LIST_TAG();
+int parse_STMT_LIST();
+int parse_STMT_LIST_TAG();
+int parse_STMT();
+int parse_VAR_OR_CALL();
+int parse_IF_STMT();
+int parse_ARGS();
+
+int parse_RETURN_STMT();
+int parse_VAR_TAG();
+int parse_EXPR();
+int parse_CONDITION();
+int parse_ARG_LIST();

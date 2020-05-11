a4260b037f278e79b458228af47b5a7172cc2ac9
assaflerman <32983621+assaflerman@users.noreply.github.com>
2020-05-11 19:59:16 +0300

    Added expected token types array

--- Parser/Parser.c
+++ Parser/Parser.c
@@ -4,7 +4,8 @@
 Token* current_token;
 eTOKENS* current_follow;
 int current_follow_size;
-eTOKENS expected_token_type;
+eTOKENS* expected_token_types;
+int expected_token_types_size;
 FILE* parser_output_file;
 
 
@@ -17,7 +18,7 @@ int parse_Follow()
 	}
 	return flag;
 }
-void error(eTOKENS expected_token_type) {
+void error() {
 	fprintf(parser_output_file, "Expected token of type '{%d}' at line: {%d},  Actual token of type '{%d}', lexeme: '{%c}'.", expected_token_type, current_token->lineNumber, current_token->kind, current_token->lexeme);
 	do
 	{

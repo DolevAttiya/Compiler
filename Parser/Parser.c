7720ef8de6785a8408f1065d1d2513b864535c14
rotemhy <50592753+rotemhy@users.noreply.github.com>
2020-05-13 11:50:24 +0300

    Add definitions for Parse functions

--- Parser/Parser.c
+++ Parser/Parser.c
@@ -17,6 +17,7 @@ int match(eTOKENS expected_token) {
 		return 0;
 	}
 	return 1;
+}
 
 int parse_Follow()
 {
@@ -34,4 +35,124 @@ void error() {
 		current_token = next_token();
 	} while (parse_Follow() == 0 && current_token -> kind != EOF_tok);
 	back_token();
+}
+
+
+void parse_ARG_LIST() {
+	fprintf(parser_output_file, "Rule {ARG_LIST -> EXPR ARG_LIST'}");
+	parse_EXPR();
+	parse_ARG_LIST_TAG();
+}
+void parse_ARG_LIST_TAG() {
+	eTOKENS follow[] = { PARENTHESIS_CLOSE_tok };
+	current_follow = follow;
+	current_follow_size = 1;
+	current_token = next_token();
+	switch (current_token->kind)
+	{
+	case COMMA_tok:
+		fprintf(parser_output_file, "Rule {ARG_LIST' -> , EXPR ARG_LIST'}");
+		if (!match(COMMA_tok))
+			return;
+		parse_EXPR();
+		parse_ARG_LIST_TAG();
+		break;
+	case PARENTHESIS_CLOSE_tok:
+		fprintf(parser_output_file, "Rule {DIM_SIZES' -> Epsilon}");
+		back_token();
+		break;
+	default:
+		error(expected_token_type);
+		break;
+	}
+}
+void parse_RETURN_STMT() {
+	eTOKENS follow[] = { SEMICOLON_tok, CURLY_BRACKET_CLOSE_tok };
+	current_follow = follow;
+	current_follow_size = 2;
+	current_token = next_token();
+	fprintf(parser_output_file, "Rule {RETURN_STMT -> return RETURN_STMT'}");
+	if (!match(current_token))
+		return;
+	parse_RETURN_STMT_TAG();
+}
+void parse_RETURN_STMT_TAG() {
+	eTOKENS first[] = { ID_tok, INT_tok, FLOAT_tok, PARENTHESIS_OPEN_tok };
+	eTOKENS follow[] = { SEMICOLON_tok, CURLY_BRACKET_CLOSE_tok };
+	current_follow = follow;
+	current_token = next_token();
+	switch (current_token->kind)
+	{
+	case ID_tok:
+		fprintf(parser_output_file, "Rule {RETURN_STMT' -> EXPR}");
+		break;
+	case INT_tok:
+		fprintf(parser_output_file, "Rule {RETURN_STMT' -> EXPR}");
+		break;
+	case FLOAT_tok:
+		fprintf(parser_output_file, "Rule {RETURN_STMT' -> EXPR}");
+		break;
+	case PARENTHESIS_OPEN_tok:
+		fprintf(parser_output_file, "Rule {RETURN_STMT' -> EXPR}");
+		break;
+		case
+		default:
+			error(expected_token_type);
+			break;
+	}
+	fprintf(parser_output_file, "Rule {RETURN_STMT' -> ε | EXPR}");
+}
+void parse_VAR_TAG() {
+	fprintf(parser_output_file, "Rule {VAR' -> [EXPR_LIST]}");
+	fprintf(parser_output_file, "Rule {VAR' -> Epsilon}");
+}
+void parse_EXPR_LIST() {
+	fprintf(parser_output_file, "Rule {EXPR_LIST -> EXPR EXPR_LIST'}");
+	parse_EXPR();
+	parse_EXPR_LIST_TAG();
+}
+void parse_EXPR_LIST_TAG() {
+	fprintf(parser_output_file, "Rule {EXPR_LIST' -> , EXPR EXPR_LIST'}");
+	fprintf(parser_output_file, "Rule {EXPR_LIST' -> Epsilon}");
+}
+void parse_CONDITION() {
+	fprintf(parser_output_file, "Rule {CONDITION -> EXPR rel_op EXPR}");
+	parse_EXPR();
+	current_token = next_token();
+	if ((current_token->kind == LESS_tok) || (current_token->kind == LESS_EQUAL_tok) || (current_token->kind == EQUAL_tok) || (current_token == GREATER_tok) || (current_token == GREATER_EQUAL_tok) || (current_token->kind == NOT_EQUAL_tok))
+		parse_EXPR();
+	else
+	{
+		eTOKENS expected_token_types[] = {LESS_tok, LESS_EQUAL_tok, EQUAL_tok, GREATER_tok, GREATER_EQUAL_tok, NOT_EQUAL_tok };
+		expected_token_types_size = 6;
+		error();
+	}
+}
+void parse_EXPR() {
+	fprintf(parser_output_file, "Rule {EXPR -> TERM EXPR'}");
+	parse_TERM();
+	parse_EXPR_TAG();
+}
+void parse_EXPR_TAG() {
+	fprintf(parser_output_file, "Rule {EXPR' -> + TERM EXPR'}");
+	fprintf(parser_output_file, "Rule {EXPR' -> Epsilon}");
+}
+void parse_TERM() {
+	fprintf(parser_output_file, "Rule {TERM -> FACTOR TERM'}");
+	parse_FACTOR();
+	parse_TERM_TAG();
+}
+void parse_TERM_TAG() {
+	fprintf(parser_output_file, "Rule {TERM' ->  * FACTOR TERM'}");
+	fprintf(parser_output_file, "Rule {TERM' ->  Epsilon}");
+}
+void parse_FACTOR() {
+	fprintf(parser_output_file, "Rule {FACTOR -> id VAR_OR_CALL'}");
+	fprintf(parser_output_file, "Rule {FACTOR -> int_num}");
+	fprintf(parser_output_file, "Rule {FACTOR -> float_num}");
+	fprintf(parser_output_file, "Rule {FACTOR -> (EXPR)}");
+}
+void parse_VAR_OR_CALL_TAG() {
+	fprintf(parser_output_file, "Rule {VAR_OR_CALL' -> VAR'}");
+	fprintf(parser_output_file, "Rule {VAR_OR_CALL' -> (ARGS)}");
 }

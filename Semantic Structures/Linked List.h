4c598fefe2c4832d46a95c1b269a472c290ff484
Dolev Attiya <50592453+DolevAttiya@users.noreply.github.com>
2020-06-04 22:20:19 +0300

    linked list with few errors
    equlity errors

---
+++ Semantic Structures/Linked List.h
@@ -0,0 +1,37 @@
+#include "Hashmap.h"
+#ifndef __LISTDEQUE_H
+#define __LISTDEQUE_H
+
+# ifndef TYPE
+# define TYPE      hashMap
+# define TYPE_SIZE sizeof(hashMap)
+# endif
+# ifndef LT
+# define LT(A, B) ((A) < (B))
+# endif
+
+# ifndef EQ
+# define EQ(A, B) ((A) == (B))
+# endif
+
+struct linkedList;
+
+struct linkedList* createLinkedList();
+
+/* Deque Interface */
+int 	isEmptyList(struct linkedList* lst);
+void  addBackList(struct linkedList* lst, TYPE e);
+void 	addFrontList(struct linkedList* lst, TYPE e);
+
+TYPE  frontList(struct linkedList* lst);
+TYPE 	backList(struct linkedList* lst);
+
+void  removeFrontList(struct linkedList* lst);
+void 	removeBackList(struct linkedList* lst);
+
+/*Bag Interface */
+void addList(struct linkedList* lst, TYPE v);
+int containsList(struct linkedList* lst, TYPE e);
+void removeList(struct linkedList* lst, TYPE e);
+
+#endif
